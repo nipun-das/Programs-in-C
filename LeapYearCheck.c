b688269475829c93bf5c850858a48caa88659939
C C Nipun Das <78753058+nipundas-github@users.noreply.github.com>
2021-06-06 13:51:48 +0530

    Add files via upload

---
+++ LeapYearCheck.c
@@ -0,0 +1,19 @@
+/*LEAP YEAR CHECK*/
+
+#include <stdio.h>
+void main()
+{
+    int y;
+    printf("enter the year: ");
+    scanf("%d",&y);
+    if((y%4==0 && y%100!=0)||(y%400==0))
+    {
+    printf("%d is a leap year",y);
+    }
+    else
+    {
+        printf("%d is not a leap year",y);
+    }
+
+
+}
