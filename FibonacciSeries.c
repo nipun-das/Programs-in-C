5c1e15c801c93e6a4dc41c29519a15511da307f0
C C Nipun Das <78753058+nipundas-github@users.noreply.github.com>
2021-06-06 13:56:10 +0530

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -3,15 +3,14 @@
 #include <stdio.h>
 void main()
 {
-    int i,n,a=-1,b=1,c;
+    int i, n, a = -1, b = 1, c;
     printf("Enter the limit: ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-        c=a+b;
-        printf("\n%d",c);
-        a=b;
-        b=c;
-    } 
-
+        c = a + b;
+        printf("\n%d", c);
+        a = b;
+        b = c;
+    }
 }
