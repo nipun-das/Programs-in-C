da4c0150e2c84151c449941352c50a81f2c80292
C C Nipun Das <78753058+nipundas-github@users.noreply.github.com>
2021-06-06 13:55:39 +0530

    Add files via upload

---
+++ FibonacciSeries.c
@@ -0,0 +1,17 @@
+/*FIBONACCI SERIES*/
+
+#include <stdio.h>
+void main()
+{
+    int i,n,a=-1,b=1,c;
+    printf("Enter the limit: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        c=a+b;
+        printf("\n%d",c);
+        a=b;
+        b=c;
+    } 
+
+}
