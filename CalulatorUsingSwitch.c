78e929608e100913567f371601d5527fad1e34da
C C Nipun Das <78753058+nipundas-github@users.noreply.github.com>
2021-06-07 11:12:37 +0530

    Add files via upload

---
+++ CalulatorUsingSwitch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    char operator;
+    double a, b;
+    printf("Enter an operator (+, -, *,): ");
+    scanf("%c", &operator);
+    printf("Enter two inputs: ");
+    scanf("%lf %lf", &a, &b);
+
+    switch (operator)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", a,b ,a+b);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", a,b,a-b);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", a,b,a*b);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", a,b,a/b);
+        break;
+        
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
