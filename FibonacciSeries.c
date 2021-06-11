ed2d4e8d90d02f245f6fa55076b5aa73604c6ecd
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-11 10:06:50 +0530

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 void main()
 {
-    int i, n, a = -1, b = 1, c;
+    int i, n, a = -1, b = 1, c;  /*Gave a from -1 and b from 1 to start the fibonacci numbers from 0 */
     printf("Enter the limit: ");
     scanf("%d", &n);
     for (i = 1; i <= n; i++)
