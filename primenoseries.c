a9c243c19e40fcd7f387670219509ee51686ecff
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-11 22:23:37 +0530

    Update primenoseries.c

--- primenoseries.c
+++ primenoseries.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 void main()
 {
-    int n,l,u,i,j,f=1;
+    int n, l, u, i, j, f = 1;
     printf("Enter the lower limit: ");
     scanf("%d", &l);
     printf("Enter the upper limit: ");
     scanf("%d", &u);
-    for (i = l+1; i <= u-1; i++)
+    for (i = l + 1; i <= u - 1; i++)      /*Since prime numbers between two limits is to printed, setting i = l + 1 and i <= u - 1  */
     {
-        f = 1;
-        for (j =2; j <= i/2; j++)
+        f = 1;                            /*Setting flag as 1 to assume its a prime number*/
+        for (j = 2; j <= i / 2; j++)
         {
-            if(i%j==0)
+            if (i % j == 0)
             {
-                f=0;
+                f = 0;                    /*Setting flag as 0 when its not a prime number*/
                 break;
             }
         }
-    if(f==1)
-    {
-    printf("%d\t",i);    
-    }
+        if (f == 1)                       /*Calling flag 1 to print everytime in the loop as prime numbers*/
+        {
+            printf("%d\t", i);
+        }
     }
 }
