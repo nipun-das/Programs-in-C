c2cd95f637f7b7ae171aed4f58b184898076331d
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-12 13:52:44 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+    int row, column, no_of_rows;
+    printf("Enter the no. of rows: ");
+    scanf("%d", &no_of_rows);
+
+    /*for (row = 1; row <= no_of_rows; row++)
+    {
+        for (column = 1; column <= row; column++)
+        {
+            printf("* ");
+        }
+    printf("\n");*/
+
+    for (row = no_of_rows; row >= 1; row--)
+    {
+        for (column = 1; column <= row; column++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
