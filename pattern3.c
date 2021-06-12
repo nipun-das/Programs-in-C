72e2412b1933fc871ba2f078c5ad7ba0557c3442
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-12 13:59:51 +0530

    Update pattern3.c

--- pattern3.c
+++ pattern3.c
@@ -4,6 +4,8 @@ void main()
     int row, column, no_of_rows;
     printf("Enter the no. of rows: ");
     scanf("%d", &no_of_rows);
+    
+    /*Combining pattern1 and pattern2 and making small changes gives me this pattern*/
 
     for (row = 1; row <= no_of_rows; row++)
     {
@@ -13,7 +15,7 @@ void main()
         }
         printf("\n");
     }
-    for (row = no_of_rows - 1; row >= 1; row--)
+    for (row = no_of_rows - 1; row >= 1; row--) /*Notice that I have started from no_of_rows-1 because without that intialization, the pattern looks unsymmetrical at the centre row*/
     {
         for (column = 1; column <= row; column++)
         {
