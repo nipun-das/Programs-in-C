463645c070d897371c7917b3db6f09de586fb8ed
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-12 13:57:09 +0530

    Update pattern2.c

--- pattern2.c
+++ pattern2.c
@@ -4,15 +4,18 @@ void main()
     int row, column, no_of_rows;
     printf("Enter the no. of rows: ");
     scanf("%d", &no_of_rows);
-
-    /*for (row = 1; row <= no_of_rows; row++)
+    
+    /*Below comment is the for loop for pattern1.c*/
+    
+    /*for (row = 1; row <= no_of_rows; row++)          
     {
         for (column = 1; column <= row; column++)
         {
             printf("* ");
         }
-    printf("\n");*/
-
+    printf("\n");*/  
+    
+    /*Notice the changes that I have made to reverse the pattern*/
     for (row = no_of_rows; row >= 1; row--)
     {
         for (column = 1; column <= row; column++)
