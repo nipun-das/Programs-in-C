4f77e56a7dfb8db809439264294dd2918d367f82
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-13 13:53:14 +0530

    Update pattern7.c

--- pattern7.c
+++ pattern7.c
@@ -1,7 +1,24 @@
+/*Pattern7*/
+/*
+for eg.for 5 rows
+
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D
+A B C
+A B
+A
+
+*/
+
+
 #include <stdio.h>
 void main()
 {
-    int ch  = 64;
+    int ch  = 64;   /*Intialized ASCII 64*/
     int row, column, no_of_rows;
     printf("Enter the no. of rows: ");
     scanf("%d", &no_of_rows);
@@ -10,7 +27,7 @@ void main()
     {
         for (column = 1; column <= row; column++)
         {
-            printf("%c ", ch + column);
+            printf("%c ", ch + column); /*To start from A, ch + column gives 65*/
         }
         printf("\n");
     }
