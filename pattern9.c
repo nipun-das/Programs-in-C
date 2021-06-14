49f2662c835c5ac71c2e5da120b55587c7e6d74f
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-14 17:36:29 +0530

    Update pattern9.c

--- pattern9.c
+++ pattern9.c
@@ -38,3 +38,19 @@ void main()
         printf("\n");
     }
 }
+
+
+/*Note: if %2d instead of %02d is written, output will be
+
+ 1 
+ 2  3
+ 4  5  6
+ 7  8  9 10
+11 12 13 14 15
+16 17 18 19
+20 21 22
+23 24
+25
+
+*/
+  
