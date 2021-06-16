be8c32fe6448311cf52f545b24bee677542a7017
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-16 10:13:04 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int f=1,n,i;
+    printf("Enter the Number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+printf("The Factorial of %d is %d",n,f);
+}
