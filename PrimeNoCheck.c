36ce2fd36079e2f51c8ca69267566af67fd1c9e6
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-16 11:59:39 +0530

    Update PrimeNoCheck.c

--- PrimeNoCheck.c
+++ PrimeNoCheck.c
@@ -1,4 +1,4 @@
-/*CHECK FOR PRIME NUMBERS*/
+//CHECK FOR PRIME NUMBERS
 
 #include <stdio.h>
 void main()
@@ -7,18 +7,18 @@ void main()
     printf("Enter the Number ");
     scanf("%d", &n);
     
-    /*1 AND 0 ARE NEITHER PRIME NOR COMPOSITES SO DEFINE THEIR CASES FIRST*/
+    //1 AND 0 ARE NEITHER PRIME NOR COMPOSITES SO DEFINE THEIR CASES FIRST
     if (n == 1 || n == 0)
     {
         printf("\n%d is Neither Prime nor Composite", n);
     }
     
-    /*FOR NUMBERS STARTING FROM 2*/
+    //FOR NUMBERS STARTING FROM 2
     else
     {
-        for (i = 2; i <= n / 2; i++)   /*HERE, i<=n/2 instead of i<=n can be used as it still valid*/
-                                       /*Input Number is checked whether it is divisible by counter i till input/2*/
-                                       /*If it is divisible by any one in that range then the input is not Prime!*/
+        for (i = 2; i <= n / 2; i++)   //HERE, i<=n/2 instead of i<=n can be used as it still valid
+                                       //Input Number is checked whether it is divisible by counter i till input/2
+                                       //If it is divisible by any one in that range then the input is not Prime!
         {
             if (n % i == 0)
             {
