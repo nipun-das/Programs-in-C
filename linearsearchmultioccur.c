ae587ca348e9790ebf386f049809423c89bc6d37
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-06-19 10:15:54 +0530

    Update linearsearchmultioccur.c

--- linearsearchmultioccur.c
+++ linearsearchmultioccur.c
@@ -3,22 +3,25 @@
 #include <stdio.h>
 void main()
 {
-    int n, a[10], i = 0, f = 0;
+    int n, m, i = 0, f = 0;
+    printf("Enter the no. of elements: ");
+    scanf("%d", &m);
+
+    int a[m];
     printf("Enter the elements: ");
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < m; i++)
     {
         scanf("%d", &a[i]);
     }
     printf("Enter the number you want to search for: ");
     scanf("%d", &n);
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < m; i++)
     {
-        f=0;
+        f = 0;
         if (a[i] == n)
         {
-        f = 1;
-        printf("Your Number %d is at location %d\n", n, i);
-        
+            f = 1;
+            printf("Your Number %d is at location %d\n", n, i);
         }
     }
     if (f == 0)
