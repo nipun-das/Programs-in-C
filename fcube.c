02ae1e82a3bcb6ecaf306cfc6826eaaacf99c411
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-08-17 11:23:57 +0530

    Add files via upload

---
+++ fcube.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int cube(int);
+int main()
+{
+    int x, r;
+    printf("Enter the Number:\n");
+    scanf("%d", &x);
+    r = cube(x);
+    printf("Cube is %d", r);
+    return 0;
+}
+int cube(int a)
+{
+    int c;
+    c = (a * a * a);
+    return c;
+}
