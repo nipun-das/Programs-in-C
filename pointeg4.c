a799f6dc9f32816531772b20f96994d18b5ee732
C C Nipun Das <78753058+Nipun-Das@users.noreply.github.com>
2021-09-15 21:23:06 +0530

    Add files via upload

---
+++ pointeg4.c
@@ -0,0 +1,24 @@
+//Sum of Elements of an Array
+
+#include <stdio.h>
+void main()
+{
+    int a[20], n, i, *p, sum = 0;
+    p = a; //Pointer Initialization
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+
+    //Address of (ptr+i)
+    //Value stored at the address *(ptr+i)
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", (p + i)); //No need to give &(p+i)
+    }
+    printf("The Array elements are:\n");
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + *(p + i);
+    }
+    printf("sum = %d\n", sum);
+}
