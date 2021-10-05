6d69735c70dabf696be1581f37dcf893ab72ccc0
C C Nipun Das <nipundas02@gmail.com>
2021-10-05 19:32:39 +0530

    Update lexicograpchisort.c

--- lexicograpchisort.c
+++ lexicograpchisort.c
@@ -1,5 +1,4 @@
-/*In mathematics, the lexicographic or lexicographical order is a generalization of the alphabetical order of the dictionaries to sequences of ordered symbols or,
-more generally, of elements of a totally ordered set.*/
+// Lexicographic sorting is the way of sorting words based on the alphabetical order of their component letters.
 
 #include <stdio.h>
 #include <string.h>
@@ -7,26 +6,31 @@ void main()
 {
     char str[20][20], temp[20];
     int n, i, j;
-    printf("Enter the number of strings:\n");
+    printf("Enter the Number of Strings:\n");
     scanf("%d", &n);
-    printf("Enter the strings:\n");
+
+    // Getting strings input
+    printf("Enter the Strings:\n");
     for (i = 0; i < n; i++)
     {
         scanf("%s", str[i]);
     }
+
+    // storing strings in the lexicographical order
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - 1 - i; j++)
         {
             if (strcmp(str[j], str[j + 1]) > 0)
             {
+                // swapping strings if they are not in the lexicographical order
                 strcpy(temp, str[j]);
                 strcpy(str[j], str[j + 1]);
                 strcpy(str[j + 1], temp);
             }
         }
     }
-    printf("Strings in the lexicographical order is:\n");
+    printf("Strings in the Lexicographical Order is:\n");
     for (i = 0; i < n; i++)
     {
         puts(str[i]);
