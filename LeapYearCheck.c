869543e7361bef5e68e6e394a5a7137253e35c3f
C C Nipun Das <nipundas02@gmail.com>
2021-11-01 18:55:26 +0530

    Update LeapYearCheck.c

--- LeapYearCheck.c
+++ LeapYearCheck.c
@@ -5,15 +5,13 @@ void main()
 {
     int y;
     printf("enter the year: ");
-    scanf("%d",&y);
-    if((y%4==0 && y%100!=0)||(y%400==0))
+    scanf("%d", &y);
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
     {
-    printf("%d is a leap year",y);
+        printf("%d is a leap year", y);
     }
     else
     {
-        printf("%d is not a leap year",y);
+        printf("%d is not a leap year", y);
     }
-
-
 }
